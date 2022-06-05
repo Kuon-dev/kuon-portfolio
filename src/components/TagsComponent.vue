c24c76985696bacf447ba797e16cfe6335d57edb
Kuon-dev <aaronlyn88@gmail.com>
2022-06-05 21:57:34 +0800

    refactor: prettier setup and tag components

---
+++ src/components/TagsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+	props: {
+		tagSettings: {
+			type: Array,
+			required: false,
+		},
+	},
+
+	mounted() {
+		console.log(tagSettings);
+	},
+};
+</script>
+
+<style></style>
